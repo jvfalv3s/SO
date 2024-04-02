f3dec368b81c8a237e7e83b6065ebca8cf0c0cc5
Joao Alves <jvfalves@gmail.com>
2024-04-02 02:18:32 +0100

    "modelo de script"

--- Projeto/script.sh
+++ Projeto/script.sh
@@ -4,7 +4,9 @@ set -xe
 n=10
 for i in $(seq 1 $n)
 do
-
+var=$(seq 1 30 | sort --random-sort | head -1)
+[02:12]
+var=$(seq start end | sort --random-sort | head -1)
+./bin $var
 ./mobileUser/mobileUser
 done
-
