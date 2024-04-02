a01a01d3895c16f061e882a04ad2f21658082cf8
Joao Alves <jvfalves@gmail.com>
2024-04-02 02:50:35 +0100

    "tentativa de concertar o make"

--- Projeto/script.sh
+++ Projeto/script.sh
@@ -11,5 +11,5 @@ MUSIC_Interval=$(seq 1 20 | sort --random-sort | head -1)
 SOCIAL_Interval=$(seq 1 20 | sort --random-sort | head -1)
 Data_to_Reserve=$(seq 1 20 | sort --random-sort | head -1)
 
-./MobileUser $Initial_Plafond $Max_Autorizations_Requests $VIDEO_Interval $MUSIC_Interval $SOCIAL_Interval $Data_to_Reserve
+./MobileUserExe $Initial_Plafond $Max_Autorizations_Requests $VIDEO_Interval $MUSIC_Interval $SOCIAL_Interval $Data_to_Reserve
 done
