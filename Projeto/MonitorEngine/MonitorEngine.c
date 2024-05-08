ede24ef7ca2328b25094a0b70bd7559b293bec82
jvfalv3s <jvfalves@gmail.com>
2024-05-08 20:24:19 +0100

    "manutencao do Monitor engine e ARM"

--- Projeto/MonitorEngine/MonitorEngine.c
+++ Projeto/MonitorEngine/MonitorEngine.c
@@ -4,35 +4,57 @@
  * --> Gonçalo José Carrajola Gaio           Nº: 2022224905
  * --> João Vitor Fraga Maia Alves           Nº: 2016122878
  **********************************************************/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <signal.h> 
 #include <mqueue.h>
+#include <semaphore.h>
+#include "MonitorEngine.h"
 
-/* Path of the MESSAGE QUEUE */
+#define MAX_SIZE 1024
 #define MESSAGE_QUEUE_PATH "../tmp/FIFO/message_queue"
-/* Path of the NAMED SEMAPHORES */
 #define NAMED_SEMAPHORES_PATH "../tmp/FIFO/NAMED_SEMAPHORES/"
 
-/* Initializing some usefull variables */
 mqd_t mq;
 struct mq_attr attr;
-sem_t *sem
+sem_t *sem;
+pid_t SYS_PID;
 
-/**
- * Creates the Monitor Engine process.
- */
-void MonEng(pid_t sys_pid) {
-    attr.mq_flags = 0;
-    attr.mq_maxmsg = 10; // Maximum number of messages in the queue
-    attr.mq_msgsize = MAX_SIZE;
-    attr.mq_curmsgs = 0;
+void MonEng(pid_t SYS_PID) {
+    // Inicialização da fila de mensagens
+    mq = mq_open(MESSAGE_QUEUE_PATH, O_CREAT | O_RDWR, 0666, &attr);
+    if (mq == -1) {
+        perror("mq_open");
+        exit(EXIT_FAILURE);
+    }
 
-    
-    exit(EXIT_SUCCESS);
+    // Inicialização do semáforo nomeado
+    sem = sem_open(NAMED_SEMAPHORES_PATH, O_CREAT | O_RDWR, 0666, 1);
+    if (sem == SEM_FAILED) {
+        perror("sem_open");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        // Monitoramento contínuo do consumo de dados
+        // Código para ler o consumo de dados de cada usuário e comparar com os limites
+
+        // Geração de alertas
+        if (consumo_critico) {
+            // Envio de mensagem de alerta para a fila de mensagens
+            char msg[MAX_SIZE];
+            sprintf(msg, "Usuário %d: consumo de dados crítico", id_usuario);
+            mq_send(mq, msg, strlen(msg) + 1, 0);
+        }
+
+        // Compilação de estatísticas
+        // Código para compilar estatísticas periódicas
+    }
+}
+
+int main() {
+    MonEng(SYS_PID);
+    return 0;
 }
--- Projeto/MonitorEngine/MonitorEngine.h
+++ Projeto/MonitorEngine/MonitorEngine.h
@@ -8,9 +8,21 @@
 #ifndef MONITORENGINE_H
 #define MONITORENGINE_H
 
-/**
- * Creates the Monitor Engine process.
- */
-void MonEng(pid_t sys_pid);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <mqueue.h>
+#include <semaphore.h>
 
+#define MAX_SIZE 1024
+#define MESSAGE_QUEUE_PATH "../tmp/FIFO/message_queue"
+#define NAMED_SEMAPHORES_PATH "../tmp/FIFO/NAMED_SEMAPHORES/"
+
+extern mqd_t mq;
+extern sem_t *sem;
+extern pid_t SYS_PID;
+
+void MonEng(pid_t SYS_PID);
 #endif
