ec8d8b7f5daa30d56f119f5b057cca98b04bee79
Gaio <goncalogaio12@gmail.com>
2024-05-13 14:38:58 +0100

    Test

--- Projeto/MonitorEngine/MonitorEngine.c
+++ Projeto/MonitorEngine/MonitorEngine.c
@@ -60,26 +60,35 @@ void MonEng() {
 void process_alerts() {
     float plafond_percentage;
     while (1) {
-        // Monitoramento contínuo do consumo de dados
-        // Código para ler o consumo de dados de cada usuário e comparar com os limites
-
-        // Geração de alertas
-        if (consumo_critico) {
-            // Envio de mensagem de alerta para a fila de mensagens
-            char msg[MAX_SIZE];
-            sprintf(msg, "Usuário %d: consumo de dados crítico", id_usuario);
-            mq_send(mq, msg, strlen(msg) + 1, 0);
+        // sem_wait();
+        for(int i = 0; i < shm_ptr->n_users; i++) {
+            plafond_percentage = (float)shm_ptr->users[i].plafond / user.max_plafond;
+            message.msg_type = shm_ptr->users[i].id;
+            if (plafond_percentage >= 100) {
+                // Plafond de 100% atingido para este usuário
+                sprintf(message.msg_text, "Alerta: Seu plafond de dados foi totalmente utilizado.");
+                msgsnd(mq_id, &message, sizeof(message), 0);
+            } else if (plafond_percentage >= 90) {
+                // Plafond de 90% ou mais atingido para este usuário
+                sprintf(message.msg_text, "Alerta: Seu plafond de dados está quase totalmente utilizado.");
+                msgsnd(mq_id, &message, sizeof(message), 0);
+            } else if (plafond_percentage >= 80) {
+                // Plafond de 80% ou mais atingido para este usuário
+                sprintf(message.msg_text, "Alerta: Seu plafond de dados está quase 80%% utilizado.");
+                msgsnd(mq_id, &message, sizeof(message), 0);
+            }
         }
-
-        // Compilação de estatísticas
-        // Código para compilar estatísticas periódicas
-
-    
-
+        // sem_wait();
     }
 }
 
-int main() {
-    MonEng(SYS_PID);
-    return 0;
-}
+/*
+criar um semáforo nomeado para controlar o acesso à fila de mensagens e à memória compartilhada
+inicializar o semáforo com o valor 1
+criar um sinal para o monitoramento do consumo de dados
+criar um sinal para o envio de alertas
+criar um sinal para a compilação de estatísticas
+criar um sinal para a finalização do processo
+mandar ao log e ao terminal a mensagem de que o Monitor Engine foi criado e o plafond de cada usuário nos parametros esperados
+no caso 80%, 90% e 100% do plafond
+*/
